01090fa1965f1f5e6f2e13b8e57b35c2850ac304
Aman <amanjaiswal144181@gmail.com>
2024-09-12 01:14:36 +0530

    Desktop popup added

--- src/main/java/com/notifier/NotificationService.java
+++ src/main/java/com/notifier/NotificationService.java
@@ -1,18 +1,40 @@
 package com.notifier;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.awt.*;
 import java.io.IOException;
 
 @Service
+@Slf4j
 public class NotificationService {
 
     public void showMacNotification(String messageContent){
+
+//        if(SystemTray.isSupported()){
+//            try {
+//                SystemTray tray = SystemTray.getSystemTray();
+//                Image image = Toolkit.getDefaultToolkit().createImage("icon.png");
+//
+//                TrayIcon trayIcon = new TrayIcon(image, "Notification");
+//                trayIcon.setImageAutoSize(true);
+//                trayIcon.setToolTip("Notification Demo");
+//                tray.add(trayIcon);
+//
+//                trayIcon.displayMessage("title", messageContent, TrayIcon.MessageType.INFO);
+//
+//            } catch (AWTException e) {
+//                e.printStackTrace();
+//            }
+//        }else {
+//            System.out.println("System tray is not supported!");
+//        }
+//
         try {
-            String[] cmd ={
-                    "osascript", "-e", "display notification \"" + messageContent + "\" with title \"New Pub/Sub Message\""
-            };
-            Runtime.getRuntime().exec(cmd);
+            String notificationCommand = String.format("terminal-notifier -message \"%s\" -title \"%s\"", messageContent, "NA");
+            Runtime.getRuntime().exec(notificationCommand);
+            log.info("Notification gotcha");
         } catch (IOException e) {
             e.printStackTrace();
         }
